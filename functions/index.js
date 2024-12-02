5e0c8d69c623a7d554f5e04bc75e2d67b3916354
Hy Nguyen <hynguyen2610@gmail.com>
2024-12-02 11:21:26 +0700

    add firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,23 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// Import function triggers from their respective submodules
+const {onRequest, onCall} = require("firebase-functions/v2/https");
+const admin = require('firebase-admin');
+const { response } = require("express");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Initialize Firebase Admin SDK
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Define your Cloud Function using v2 syntax
+exports.randomNumber = onRequest((request, response) => {
+    console.log('Random number called');
+    const number = Math.round(Math.random() * 100);
+    response.send(number.toString());
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.toTheDojo = onRequest((request, response) => {
+    console.log("Redirect to dojo called");
+    response.redirect('https://thenetninja.co.uk');
+});
+
+exports.sayHello = onCall((data, context) => {
+    return `Hello`;
+});
