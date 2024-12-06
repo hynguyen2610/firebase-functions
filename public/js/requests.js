701a65eb9a0852e36a06e0418d7bf349e7e9e0f7
Hy Nguyen <hynguyen2610@gmail.com>
2024-12-06 11:30:10 +0700

    use Vue to render list of Requests

--- public/js/requests.js
+++ public/js/requests.js
@@ -3,31 +3,15 @@ const ref = firebase.firestore().collection('requests');
 const app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello!'
+      // read the requests from firebase
+      requests: []
+    },
+    mounted() {
+      ref.onSnapshot(snapshot => {
+        this.requests = [];
+        snapshot.forEach(doc => {
+          this.requests.push({ ...doc.data(), id: doc.id });
+        });
+      });
     }
   });
-
-ref.onSnapshot(snapshot => {
-    // console.log(snapshot);
-
-    let requests = [];
-    snapshot.forEach(doc => {
-        requests.push({ ...doc.data(), id: doc.id });
-    });
-    // console.log(requests);
-
-    let html = '';
-    requests.forEach(request => {
-        html += `
-      <li>
-        <span class="text">${request.text}</span>
-        <div>
-          <span class="votes">${request.upvotes}</span>
-          <i class="material-icons upvote">arrow_upward</i>
-        </div>
-      </li>
-    `;
-    });
-    document.querySelector('ul').innerHTML = html;
-    console.log(requests);
-});
