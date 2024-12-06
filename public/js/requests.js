281025eb03f83e9b8e66332c5d526e3de0d735e4
Hy Nguyen <hynguyen2610@gmail.com>
2024-12-06 15:46:21 +0700

    Sort the requests bases on votes

--- public/js/requests.js
+++ public/js/requests.js
@@ -7,11 +7,12 @@ const app = new Vue({
       requests: []
     },
     mounted() {
-      ref.onSnapshot(snapshot => {
+        // sort the requests by upvotes
+      ref.orderBy('upvotes', 'desc').onSnapshot(snapshot => {
         this.requests = [];
         snapshot.forEach(doc => {
           this.requests.push({ ...doc.data(), id: doc.id });
         });
-      });
+      })
     }
   });
